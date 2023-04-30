29c1e113b949f9d717816ac6390271b96b473509
Bus-sen <snbusra66@gmail.com>
2023-04-30 22:58:12 +0300

    app colors added

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors{ //P1 30:55:00
+  static final Color textColor = Color.fromRGBO(58, 90, 64, 1);
+  static final Color mainColor = Color.fromRGBO(111, 154, 141, 1);
+  static final Color iconColor1 = Color.fromRGBO(250, 152, 132 ,1);
+  static final Color iconColor2 = Color.fromRGBO(247, 208, 96, 1);
+
+}
